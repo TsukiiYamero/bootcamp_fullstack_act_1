c73ef6f85c773a678442ccdf8b379c57fda3f032
SaiKisho <diegosferas@gmail.com>
2024-05-16 19:42:21 -0500

    ⭐ Base project for gallery

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-
-      <h1>Hello World - Diego Bello 👋</h1>
-
-      <div className="card">
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      hola
+    </div>
   )
 }
 
