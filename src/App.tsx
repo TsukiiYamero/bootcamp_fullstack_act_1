82aa64e84de5e4112c0e149b3f99f944cb579da6
SaiKisho <diegosferas@gmail.com>
2024-05-24 19:58:35 -0500

    hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
 
 import './App.css'
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom';
-import { Gallery } from '@/components/utils/gallery';
-import { Navigation } from './components/Navigation';
+import { FirstComponent } from './components/FirstComponent'
+import { SecondComponent } from './components/SecondComponent'
 
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <RoutesGallery />
-      <Navigation />
-    </BrowserRouter>
-  )
-}
-
-
-const RoutesGallery = () => {
-  return (
-    <Routes>
-      {
-        Gallery.map(({ path, img, title }) => (
-          <Route
-            key={path}
-            path={path}
-            element={<ViewImage img={img} title={title} />}
-          />
-        ))
-      }
-    </Routes>
-  )
-}
-
-const ViewImage = ({ img, title }: { img: string, title: string, description?: string }) => {
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', marginInline: 'auto', alignItems: 'center', width: '700px', height: '500px', gap: '1rem' }}>
-      <img style={{ width: '100%', objectFit: 'cover', borderRadius: '9px' }} src={img} alt={title} />
-      <h1 style={{ textAlign: 'center', fontSize: '2.5rem', color: 'white', margin: 0 }}>{title}</h1>
+    <div>
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
     </div>
   )
 }
--- src/components/images/AcheronImg.tsx
+++
@@ -1,9 +0,0 @@
-import AcheronImage from '@/assets/img/acheron-honkai-star-rail-game-hd-wallpaper-uhdpaper.webp';
-
-export const AcheronImg = () => {
-    return (
-        <div>
-            <img src={AcheronImage} alt="Image of Acheron" />
-        </div>
-    )
-}
